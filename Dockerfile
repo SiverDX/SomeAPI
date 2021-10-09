bc1319cba055e8c61415718c7aa4dc499e7b4d5d
Cadentem <kai.zahn@yahoo.de>
2021-10-10 01:18:35 +0200

    Got Tomcat running + changed Tomcat image to JRE 11 + set tomcat dependencies to provided + set a start class + repackage the .war file with spring boot plugin + changed the java compiler version

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,11 @@ FROM maven:3.8.3-adoptopenjdk-16 AS builder
 COPY src /home/SomeAPI/src
 COPY pom.xml /home/SomeAPI
 
-RUN mvn -f /home/SomeAPI/pom.xml clean package
+RUN mvn -f /home/SomeAPI/pom.xml clean package spring-boot:repackage
 
-FROM tomcat:9.0.54-jdk8-temurin-focal
+FROM tomcat:9.0.54-jre11
 
 # Create acutal image
 LABEL maintainer="https://github.com/SiverDX"
 
-COPY --from=builder /home/SomeAPI/target/SomeAPI*.war /usr/local/tomcat/webapps
-
-EXPOSE 8081
+COPY --from=builder /home/SomeAPI/target/some-api*.war /usr/local/tomcat/webapps
--- src/main/java/de/cadentem/application/Application.java
+++ src/main/java/de/cadentem/application/Application.java
@@ -3,15 +3,22 @@ package de.cadentem.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ComponentScan("de.cadentem")
 @EntityScan("de.cadentem.entities")
 @EnableJpaRepositories("de.cadentem.repositories")
-public class Application {
-    public static void main(final String[] args) {
+public class Application extends SpringBootServletInitializer {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
